577229874b2bbc9d5ce8c0058d74d871ad81edbf
BnnQ <1v25fa94@gmail.com>
2023-07-04 19:20:58 +0300

    Added general site styles

---
+++ CommonStylesheets.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="/TravelAgency/stylesheets/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="/TravelAgency/stylesheets/site.css"/>
+<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
