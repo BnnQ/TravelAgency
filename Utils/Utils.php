8ce2ed546550a41cf94f665c94249d18007b5a6d
BnnQ <1v25fa94@gmail.com>
2023-07-08 11:16:51 +0300

    Added util methods for working with directories

--- Utils/Utils.php
+++ Utils/Utils.php
@@ -2,3 +2,7 @@
 function unsetCookie($cookieName) : void {
     setcookie($cookieName, "", time() - 1);
 }
+
+function isDirectoryEmpty($directoryPath): bool {
+    return count(glob($directoryPath . '/*')) === 0;
+}
