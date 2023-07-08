2d44401cba7c6f93ba609e233880d85c9e6b00ee
BnnQ <1v25fa94@gmail.com>
2023-07-08 11:18:49 +0300

    Enhanced RoleManager with method getUsersWithRole

--- Services/IRoleManager.php
+++ Services/IRoleManager.php
@@ -9,6 +9,12 @@ interface IRoleManager
 {
     public function getRoleById(int $id) : Role;
     public function getRoleByName(string $name) : Role;
+
+    /**
+     * @param string $roleName
+     * @return User[]
+     */
+    public function getUsersWithRole(string $roleName): array;
     public function addRole(Role $role): void;
     public function changeUserRole(User $user, Role $role): void;
     public function isUserInRole(User $user, string $roleName): bool;
--- Services/MySqlRoleManager.php
+++ Services/MySqlRoleManager.php
@@ -34,7 +34,7 @@ class MySqlRoleManager implements IRoleManager
      * @throws StatementPrepareFailedException
      * @throws QueryFailedException
      */
-    public function getRoleByName($name): Role
+    public function getRoleByName(string $name): Role
     {
         $query = "SELECT * FROM Roles WHERE Name = ?";
         $response = MySqlUtils::prepareAndGetResult($this->context, $query, 's', $name);
@@ -44,6 +44,30 @@ class MySqlRoleManager implements IRoleManager
         return $role;
     }
 
+    /**
+     * @inheritDoc
+     * @throws StatementPrepareFailedException
+     * @throws QueryFailedException
+     */
+    public function getUsersWithRole(string $roleName): array
+    {
+        $query = "SELECT Id from Roles WHERE Name = ?";
+        $response = MySqlUtils::prepareAndGetResult($this->context, $query, 's', $roleName);
+        $roleId = $response->fetch_assoc()['Id'];
+        $response->free_result();
+
+        $query = "SELECT * FROM Users WHERE RoleId = ?";
+        $response = MySqlUtils::prepareAndGetResult($this->context, $query, 'i', $roleId);
+
+        $users = [];
+        while ($row = $response->fetch_assoc()) {
+            $user = User::parseFromAssoc($row);
+            $users[] = $user;
+        }
+
+        $response->free_result();
+        return $users;
+    }
     /**
      * @throws StatementPrepareFailedException
      * @throws QueryFailedException
