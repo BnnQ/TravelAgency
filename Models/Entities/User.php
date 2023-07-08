7970e99c313551103591486627b358032c32f9f8
BnnQ <1v25fa94@gmail.com>
2023-07-08 11:15:43 +0300

    Refactored User entity

--- Models/Entities/User.php
+++ Models/Entities/User.php
@@ -4,29 +4,13 @@ namespace Models\Entities;
 
 class User
 {
-    public ?int $id;
-    public string $login;
-    public string $hashedPassword;
-    public string $email;
-    public int $roleId;
-    public float $discount;
-    public ?string $lastAuthenticationToken;
-    public ?string $avatar;
-
-    public function __construct(?int $id, string $login, string $hashedPassword, string $email, int $roleId, float $discount = 0.0, ?string $lastAuthenticationToken = null, ?string $avatar = null)
+    public function __construct(public ?int $id, public string $login, public string $hashedPassword, public string $email, public string $roleName, public float $discount = 0.0, public ?string $lastAuthenticationToken = null, public ?string $avatar = null)
     {
-        $this->id = $id;
-        $this->login = $login;
-        $this->hashedPassword = $hashedPassword;
-        $this->email = $email;
-        $this->roleId = $roleId;
-        $this->discount = $discount;
-        $this->lastAuthenticationToken = $lastAuthenticationToken;
-        $this->avatar = $avatar;
+        //empty
     }
 
     public static function parseFromAssoc(array $associativeArray) : User {
-        return new User(@$associativeArray['id'] ?? @$associativeArray['Id'], @$associativeArray['login'] ?? @$associativeArray['Login'], @$associativeArray['hashedPassword'] ?? @$associativeArray['HashedPassword'], @$associativeArray['email'] ?? @$associativeArray['Email'], @$associativeArray['roleId'] ?? @$associativeArray['RoleId'], @$associativeArray['discount'] ?? @$associativeArray['Discount'] ?? 0.0, @$associativeArray['lastAuthenticationToken'] ?? @$associativeArray['LastAuthenticationToken'], @$associativeArray['avatar'] ?? @$associativeArray['Avatar']);
+        return new User(@$associativeArray['id'] ?? @$associativeArray['Id'], @$associativeArray['login'] ?? @$associativeArray['Login'], @$associativeArray['hashedPassword'] ?? @$associativeArray['HashedPassword'], @$associativeArray['email'] ?? @$associativeArray['Email'], @$associativeArray['roleName'] ?? @$associativeArray['RoleName'], @$associativeArray['discount'] ?? @$associativeArray['Discount'] ?? 0.0, @$associativeArray['lastAuthenticationToken'] ?? @$associativeArray['LastAuthenticationToken'], @$associativeArray['avatar'] ?? @$associativeArray['Avatar']);
     }
 
 }
