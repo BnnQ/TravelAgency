eb47d328874c7eb5ddff68519958a1fe7948b8ab
BnnQ <1v25fa94@gmail.com>
2023-07-08 11:16:15 +0300

    Enhanced UserManagerBase functions

--- Services/UserManagerBase.php
+++ Services/UserManagerBase.php
@@ -3,13 +3,18 @@
 namespace Services;
 
 require_once "Utils\RouteConstants.php";
+require_once "Utils\RoleConstants.php";
 use Exceptions\UserNotFoundException;
 use Models\Entities\User;
 
 abstract class UserManagerBase
 {
+    /**
+     * @return User[]
+     */
+    public abstract function getUsers(): array;
     public abstract function getUser(string $login): User;
-    public abstract function signUpUser(string $login, string $password, string $email): void;
+    public abstract function signUpUser(string $login, string $password, string $email, string $roleName = ROLE_User, float $discount = 0.0, ?string $avatar = null): void;
     public abstract function verifyAndSignInUser(string $login, string $password): bool;
     public abstract function signInUser(string $login): void;
     public abstract function signOutUser(): void;
